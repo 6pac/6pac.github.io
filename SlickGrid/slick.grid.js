17e8766f6cfd1e814c998dc39c5d2f1bf027ed8e
Ben McIntyre <email.ben.mcintyre@gmail.com>
2018-03-04 23:06:05 +0930

    release v2.3.16 - update github.io

--- SlickGrid/slick.grid.js
+++ SlickGrid/slick.grid.js
@@ -3844,7 +3844,7 @@ if (typeof Slick === "undefined") {
     // Public API
 
     $.extend(this, {
-      "slickGridVersion": "2.4.0",
+      "slickGridVersion": "2.3.16",
 
       // Events
       "onScroll": new Slick.Event(),
